0fd373e0ed046c595d5bbac78449f0483c15c1de
Lahat <bamba34@hotmail.com>
2015-12-23 12:54:16 +0100

    Je me suis trompé de classe, rectification

--- src/service/Producteur.java
+++ src/service/Producteur.java
@@ -1,70 +1,16 @@
 package service;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.InetAddress;
-import java.net.Socket;
-
-
 public class Producteur {
-    
-    public String id;
-    public String mess;
-    public boolean autorisation;
-    
-    Socket socket;
-    InetAddress dest;
-    int portdest=4202 ;//port d'écoute du destinataire ou du serveur
-    BufferedReader in;
-    PrintStream out;
-    
 
-    public void produire() {
-    }
+	String id;
+	String mess;
+	
+	public void consommer() {}
+    public void demande() {}
 
-    public void demande() {
-    }
+	public static void surReceptionDe(String message) {}
 
-    public void envoyerA(String dest,String message) {
-    	try {
-    		this.dest=InetAddress.getByName(dest);
-			socket= new Socket(this.dest,portdest);
-			if(socket.isConnected())
-				System.out.println("Connexion établit avec l'hote :"+dest);
-				in=new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				out= new PrintStream(socket.getOutputStream());
-				out.println(message);
-				ReceptionR();
-						
-		} catch (IOException e) {
-			System.out.println("Adresse inconnue");
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    }
-    public void ReceptionR() {
-    	try {
-			String rep=in.readLine();
-			//String hote=(String)socket.getRemoteSocketAddress();
-			service.Consommateur.surReceptionDe(rep);//reste comment faire l'adresse de l'emetteur afaire
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	finally{
-    	try {
-			in.close();
-			out.close();
-	    	socket.close();
-	    	System.out.println("Déconnexion");
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	
-    	}
-    }
+    
+   
 
 }
