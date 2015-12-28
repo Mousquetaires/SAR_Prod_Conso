455c52afc22208da1d6bf545889339b409705619
Lahat <bamba34@hotmail.com>
2015-12-28 16:48:54 +0100

    Producteur coté réseau

--- src/service/Producteur.java
+++ src/service/Producteur.java
@@ -1,14 +1,45 @@
 package service;
+import reseau.*;
+
 
 public class Producteur {
-
+	
+	static boolean autorisation;
 	String id;
 	String mess;
+	reseau.Producteur p;
 	
-	public void consommer() {}
-    public void demande() {}
+	/*A definir le requete*/
+	String req = "REQ";
+	
+	public Producteur(reseau.Producteur p){
+		this.p = p;
+	}
+	public void produire(String dest, String mess) {
+		
+		while(autorisation == false){
+			System.out.println("pas de message");
+		}
+		if(autorisation == true){
+			
+			p.envoyerA(dest, mess);
+			autorisation = false;
+			
+		}
+	}
+    public void demande(String dest) {
+    	p.envoyerA(dest, req);
+    }
 
-	public static void surReceptionDe(String message){}
+	public static void surReceptionDe(String dest, String mess){
+		if(mess == "ACK"){
+			autorisation = true;
+		}else{
+			if(mess == "REFUS"){
+				System.out.println("Rien a faire");
+			}
+		}
+	}
 
     
    
