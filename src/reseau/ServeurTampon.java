9a6a7ec6e985b3b635b9d0041c97893f151d0f7a
Mansour <mansour_niang@hotmail.fr>
2015-12-28 14:01:31 +0100

    Début serveur tampon

--- src/reseau/ServeurTampon.java
+++ src/reseau/ServeurTampon.java
@@ -1,23 +1,68 @@
 package reseau;
 
+import service.*;
+
+import java.io.*;
+import java.net.*;
 
 public class ServeurTampon {
+    static ServerSocket socketServeur;
+    static Socket socketService;
+    BufferedReader in = null;
+    PrintWriter out = null;
+    static int port = 4020;
+    String msg; //pour lire un message sur le flux
+    Tampon tampon;
+    String conso;
+    String prod;
     
-    public String socketEcoute;
-
-   
-    public String socket;
-
     
-    public void envoyerA() {
+    public static void main(String[] args){
+    	try{
+			socketServeur = new ServerSocket(port);			
+        	System.out.println("Le serveur est pret a l'écoute !");
+        	
+        	while (true) {
+				socketService = socketServeur.accept();			
+    			System.out.println("La connexion est accpetée");
+    			
+    		}
+        }catch(IOException e){
+        	e.getMessage();    	
+        }
+        finally{
+        	try{
+        		socketServeur.close();
+        	}catch(IOException e){
+        		e.getMessage();
+        	}
+        }
     }
-
-   
-    public void receptionC() {
-    }
-
     
+    
+    public void envoyerA(String dest, String msg){
+    	this.out.println(msg);
+    }
+   
     public void receptionP() {
+    	while(true){
+    		try {
+				msg = this.in.readLine();
+			} catch (IOException e) {
+				e.getMessage();
+			}
+    		tampon.surReceptionDeP(prod, msg);
+    	}
+    }
+    public void receptionC() {
+    	while(true){
+    		try {
+				msg = this.in.readLine();
+			} catch (IOException e) {
+				e.getMessage();
+			}
+    		tampon.surReceptionDeC(conso, msg);
+    	}
     }
 
 }
--- src/service/Tampon.java
+++ src/service/Tampon.java
@@ -1,33 +1,50 @@
 package service;
 
+import reseau.*;
 
-
-
-public class Tampon {
-   
+public class Tampon {   
     public String id;
-
-    public String tabMess;
-
-  
-    public String nbMess;
-
-
-    public String outc;
-
-   
-    public String inp;
-
+    public String [] tabMess;  
+    public int nbMess;
+    public int outC;   
+    public int inP;
+    ServeurTampon serv;
+    String req;
     
-    public void surReceptionDeP() {
+    
+    public boolean autorisation(String producteur) {
+    	String ack = "Vous avez l'autorisation de produir un message";
+    	String refus = "Vous ne pouvez pas produir un message pour le moment";
+    	if(nbMess<tabMess.length){
+    		serv.envoyerA(producteur, ack);
+    		return true;
+    	}else{
+    		serv.envoyerA(producteur, refus);
+    		return false;
+    	}
+    }
+    
+    public void surReceptionDeP(String producteur, String msg) {
+    	if(msg != req){
+    		tabMess[inP] = msg;
+    		inP = (inP+1)%tabMess.length;
+    		nbMess++;
+    	}else{
+    		this.autorisation(producteur);
+    	}
     }
 
    
-    public void surReceptionDeC() {
-    }
-
-    
-    public void autorisation() {
+    public void surReceptionDeC(String consommateur, String msg) {
+    	String refus = "Il n'y a pas de messages dans le tampon"; 
+    	if(nbMess>0){
+    		msg = tabMess[outC];
+    		outC = (outC+1)%tabMess.length;
+    		nbMess--;
+    		serv.envoyerA(consommateur, msg);
+    	}else{
+    		serv.envoyerA(consommateur, refus);
+    	}
     }
 
 }
