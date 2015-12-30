70841e8348342af22c8c10e7bf9c77db60da8526
Lahat <bamba34@hotmail.com>
2015-12-30 12:05:31 +0100

    Producteur de la classe application

--- src/application/Producteur.java
+++ src/application/Producteur.java
@@ -1,7 +1,29 @@
 package application;
 
+import java.util.Scanner;
+
 public class Producteur {
 	
+
+	service.Producteur p;
+	service.Tampon t;
+	Scanner scan = new Scanner(System.in);
+	String tamp;
 	
+	public String saisirMessage(String mess){
+		mess = scan.nextLine();
+		return mess;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		while(true){
+			
+			
+		}
+
+	}
 
 }
